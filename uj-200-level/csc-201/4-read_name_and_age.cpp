acfa05d571308378da81a31e7e9a5d90bea44a6f
Abdulqadir Ahmad <arfs6.mail@gmail.com>
2024-12-11 11:56:14 +0100

    updated mth 204 notes.

--- uj-200-level/csc-201/4-read_name_and_age.cpp
+++ uj-200-level/csc-201/4-read_name_and_age.cpp
@@ -2,10 +2,11 @@
 #include "std_lib_facilities.h"
 
 int main() {
-	cout << "Please enter your first name and age\n";
+	cout << "Please enter your first name and age\n"; // Prompt message
 	int age; // Integer variable
-	string first_name;
-	cin >> first_name;
-	cin >> age;
+	string first_name; // string variable
+	cin >> first_name; // input into string variable
+	cin >> age; // Input into int variable
 	cout << "Hello, " << first_name << " (age " << age << ")\n";
+	// Greeting message with int variable.
 }
